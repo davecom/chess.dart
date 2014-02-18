c9ada04f2514ce9fdce9a58ad4b0097738766e6a
Kasper Lund <kasper@verdich.dk>
2014-02-18 10:14:28 +0100

    Use a benchmark harness to (more accurately) measure performance of the perft  support.

--- test/perft.dart
+++ test/perft.dart
@@ -1,24 +1,49 @@
 import "package:chess/chess.dart";
+import "package:benchmark_harness/benchmark_harness.dart";
+
+class PerftBenchmark extends BenchmarkBase {
+  final String fen;
+  final int depth;
+  final int nodes;
+  Chess chess;
+
+  PerftBenchmark(String fen, this.depth, this.nodes)
+      : this.fen = fen
+      , super("Perft(fen:'$fen')");
+
+  setup() {
+    chess = new Chess();
+    chess.load(fen);
+  }
+
+  teardown() {
+    chess = null;
+  }
+
+  run() {
+    int result = chess.perft(depth);
+    if (result != nodes) {
+      throw 'Wrong result: Expected <$nodes> but got <$result>.';
+    }
+  }
+}
 
 void main() {
-  Stopwatch stopwatch = new Stopwatch()
-  ..start();
-    List perfts = [
-    {'fen': 'r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1',
-      'depth': 3, 'nodes': 97862},
-    {'fen': '8/PPP4k/8/8/8/8/4Kppp/8 w - - 0 1',
-      'depth': 4, 'nodes': 89363},
-    {'fen': '8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1',
-      'depth': 4, 'nodes': 43238},
-    {'fen': 'rnbqkbnr/p3pppp/2p5/1pPp4/3P4/8/PP2PPPP/RNBQKBNR w KQkq b6 0 4',
-      'depth': 3, 'nodes': 23509},
-    ];
-
-    perfts.forEach((perft) {
-      Chess chess = new Chess();
-      chess.load(perft['fen']);
-  
-      var nodes = chess.perft(perft['depth']);
-    });
-  print(stopwatch.elapsedMilliseconds);
+  List perfts = [
+    new PerftBenchmark(
+        'r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1',
+        3, 97862),
+    new PerftBenchmark(
+        '8/PPP4k/8/8/8/8/4Kppp/8 w - - 0 1',
+        4, 89363),
+    new PerftBenchmark(
+        '8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1',
+        4, 43238),
+    new PerftBenchmark(
+        'rnbqkbnr/p3pppp/2p5/1pPp4/3P4/8/PP2PPPP/RNBQKBNR w KQkq b6 0 4',
+        3, 23509)
+  ];
+  for (BenchmarkBase perft in perfts) {
+    perft.report();
+  }
 }
